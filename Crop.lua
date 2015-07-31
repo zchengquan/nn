4b0b94264c29273a65e87ff50ce9629e27fb381b
zchengquan <zhang544425724@163.com>
2015-07-31 17:27:25 +0800

    fixe some bug
    the crop layer foraward input:{input_data,input}, thus the updateGradInput: {nil,gradInput}

--- Crop.lua
+++ Crop.lua
@@ -1,15 +1,15 @@
 local Crop, Parent = torch.class('nn.Crop', 'nn.Module')
----- forward: two paras, work with nnGraph. need redefine forward
 
 function Crop:__init()
 	Parent.__init(self)
 	self.mask = torch.Tensor()
 end
 
-function Crop:updateOutput(input_data,input)
--- input_data: bottom data
--- input: the current layer's input, format:nBatch x nChanel x H x W  or  nChannel x H x W
-	--self.output:resizeAs(input_data)  --:copy(input_data)
+function Crop:updateOutput(_input)
+        -- input_data: bottom data
+        -- input: current laye's input, format:nBatch x nChanel x H x W or nChannel x H x W, just one scale for per training time
+	local input_data = _input[1];
+	local input = _input[2];
 	if input_data:dim() == 3 then -- one image
 		self.output:resize(input:size(1),input_data:size(2),input_data:size(3))  --:copy(input_data)
 		self.mask = torch.Tensor(1,4)  -- pad_l, pad_r,pad_t,pad_b
@@ -18,8 +18,7 @@ function Crop:updateOutput(input_data,input)
 		self.mask[{1,3}]=math.floor((input:size(3) - input_data:size(3))/2)
 		self.mask[{1,4}]=(input:size(3) - input_data:size(3)) - self.mask[{1,3}]	
 		-- update: crop input
-		self.output:copy(input[{{},{self.mask[{1,1}]+1,self.mask[{1,1}]+input_data:size(2)},{self.mask[{1,3}]+1,self.mask[{1,3}]+input_data:size(3)}}])
-
+		self.output:copy(input[{{},{self.mask[{1,1}]+1,self.mask[{1,1}]+input_data:size(2)},{self.mask[{1,3}]+1,self.mask[{1,3}]+input_data:size(3)}}]);
 	elseif input_data:dim() == 4 then  -- batch
 		self.output:resize(input:size(1),input:size(2),input_data:size(3),input_data:size(4))  --:copy(input_data)
 		--self.mask = torch.Tensor(input_data:size(1),4) 
@@ -30,15 +29,15 @@ function Crop:updateOutput(input_data,input)
 		self.mask[{1,4}]=(input:size(4) - input_data:size(4)) - self.mask[{1,3}]
 		-- update: crop input
 		self.output:copy(input[{{},{},{self.mask[{1,1}]+1,self.mask[{1,1}]+input_data:size(3)},{self.mask[{1,3}]+1,self.mask[{1,3}]+input_data:size(4)}}])
-        else
+	else
 		error('<Crop updateOutput> illegal input, must be 3 D or 4 D')
 	end
-	-- updateOutput
+	-- update crop input
 	return self.output
 end
 
 
-function Crop:updateGradInput(input,gradOutput)
+function Crop:updateGradInput(_input,gradOutput)
 	--self.gradInput = torch.Tensor()
 	if gradOutput:dim() == 3 then
 		self.gradInput:resize(gradOutput:size(1),gradOutput:size(2)+self.mask[{1,1}]+self.mask[{1,2}],gradOutput:size(3)+self.mask[{1,3}]+self.mask[{1,4}])
@@ -51,10 +50,23 @@ function Crop:updateGradInput(input,gradOutput)
 	else
 		error('<crop updateGradInput> illegal gradOutput, must be 3 D or 4 D')
 	end 
-	return self.gradInput
+	return {nil,self.gradInput}
 end
 
-function Crop:forward(input_data, input)
--- rewrite forward, need be fixed given the input data format
-   return self:updateOutput(input_data,input)
+function Crop:accGradParameters(_input, gradOutput, scale)
 end
+
+
+function Crop:forward(_input)
+-- rewrite forward
+   return self:updateOutput(_input)
+end
+
+function Crop:backward(_input, gradOutput, scale)
+   scale = scale or 1
+   self:updateGradInput(_input, gradOutput)
+   self:accGradParameters(_input, gradOutput, scale)
+   return self.gradInput
+end
+
+
